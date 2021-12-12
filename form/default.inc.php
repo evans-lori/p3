f410d980f3a2a0beb0226ab111606e87966884da
Your Name <lorievans@ufl.edu>
2021-12-12 17:17:56 -0500

    added form file

---
+++ form/default.inc.php
@@ -0,0 +1,126 @@
+<style type="text/css">
+*{ 
+margin: 0;  
+padding: 0; 
+border: 0; 
+} 
+
+.container { 
+    max-width: 800px; 
+    margin: 0 auto; 
+    padding-top: 150pt; 
+} 
+
+h1 { 
+    font-family: 'Dancing Script', cursive; 
+    color: #F4F4F4; 
+    text-align: center; 
+    font-size: 50px; 
+    letter-spacing: 5px; 
+    margin-bottom: 50px; 
+} 
+
+p, 
+ul, 
+li, 
+a { 
+    font-family: 'EB Garamond', serif; 
+    color: #F4F4F4; 
+    font-size: 19px;
+    margin-top: 30px;
+    text-align: center; 
+} 
+
+ol { 
+    list-style-type: upper-alpha; 
+    list-style-type: upper-alpha; 
+    position: relative; 
+    top: -20px; 
+    bottom: 0; 
+    left: 50px; 
+}  
+
+body { 
+    background-color: #146093; 
+    color: #F4F4F4 
+} 
+
+header { 
+    background: #121929; 
+    padding-bottom: 10mm; 
+    position: fixed; 
+    top: 0px; 
+    width: 100%; 
+    z-index: 99; 
+} 
+
+nav { 
+    padding-left: 20pt; 
+} 
+
+nav ul li { 
+    display: inline; 
+} 
+
+nav a { 
+    text-decoration: none; 
+    color: #333; 
+    background: #f2f2f2; 
+    padding: 10px 20px; 
+    transition: .5s; 
+    width: 100px; 
+} 
+    
+nav a:hover { 
+    background:#2DA2D1; 
+    color: #f2f2f2; 
+    transition: .5s; 
+} 
+
+header h1 { 
+    width: 30%; 
+    display: inline-block; 
+} 
+
+header nav { 
+    width: 65%; 
+    display: inline-block; 
+    text-align: right; 
+} 
+
+header li { 
+    display: inline-block; 
+    margin: 0 40px; 
+} 
+
+main .container { 
+    margin: 90px auto; 
+} 
+
+footer { 
+    text-decoration: none; 
+    background: #12192940; 
+    text-align: center; 
+    padding: 2rem; 
+    margin-top: 30px;
+    width: 100%; 
+    z-index: 99; 
+} 
+</style>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+        </ul>
+    </nav> 
+</header>
+
+<div class="container">
+    <h1>Sorry. Something went wrong with your submission.</h1>
+    <p>Please try again.</p>
+</div>
+
+<footer>
+    <p>Website by Lori L. Evans</p>
+</footer>
---
+++ form/error.inc.php
@@ -0,0 +1,133 @@
+<style type="text/css">
+*{ 
+margin: 0;  
+padding: 0; 
+border: 0; 
+} 
+
+.container { 
+    max-width: 800px; 
+    margin: 0 auto; 
+    padding-top: 150pt; 
+} 
+
+h1 { 
+    font-family: 'Dancing Script', cursive; 
+    color: #F4F4F4; 
+    text-align: center; 
+    font-size: 50px; 
+    letter-spacing: 5px; 
+    margin-bottom: 50px; 
+}
+
+p, 
+ul, 
+li, 
+a { 
+    font-family: 'EB Garamond', serif; 
+    color: #F4F4F4; 
+    font-size: 19px;
+    margin-top: 30px; 
+} 
+
+ol { 
+    list-style-type: upper-alpha; 
+    list-style-type: upper-alpha; 
+    position: relative; 
+    top: -20px; 
+    bottom: 0; 
+    left: 50px; 
+}  
+
+body { 
+    background-color: #146093; 
+    color: #F4F4F4 
+} 
+
+header { 
+    background: #121929; 
+    padding-bottom: 10mm; 
+    position: fixed; 
+    top: 0px; 
+    width: 100%; 
+    z-index: 99; 
+} 
+
+nav { 
+    padding-left: 20pt; 
+} 
+
+nav ul li { 
+    display: inline; 
+} 
+
+nav a { 
+    text-decoration: none; 
+    color: #333; 
+    background: #f2f2f2; 
+    padding: 10px 20px; 
+    transition: .5s; 
+    width: 100px; 
+} 
+    
+nav a:hover { 
+    background:#2DA2D1; 
+    color: #f2f2f2; 
+    transition: .5s; 
+} 
+
+header h1 { 
+    width: 30%; 
+    display: inline-block; 
+} 
+
+header nav { 
+    width: 65%; 
+    display: inline-block; 
+    text-align: right; 
+} 
+
+header li { 
+    display: inline-block; 
+    margin: 0 40px; 
+} 
+
+main .container { 
+    margin: 90px auto; 
+} 
+
+footer { 
+    text-decoration: none; 
+    text-align: center; 
+    padding: 2rem; 
+    margin-top: 30px; 
+} 
+</style>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+        </ul>
+    </nav> 
+</header>
+
+<div class="container">
+	<h1>Missing fields</h1>
+	<p>Sorry, you have not completed all of the required fields.</p>
+	<p>Please hit <a href="#" onClick="history.go(-1)">back</a> and complete the following required fields.</p>
+
+	<ul>
+	<?php
+		for($i=0; $i<count($this->missing_required_fields); $i++){
+			echo "<li>" . $this->missing_required_fields[$i]['title'] . "</li>\n";
+		}
+	?>
+	</ul>
+
+	<p><strong><a href="#" onClick="history.go(-1)">Back to form</a></strong></p>
+<div>
+
+<footer>
+    <p>Website by Lori L. Evans</p>
+</footer>
